dba3e2db57f9491e5da6c1d61277cca80f8b69c0
zhimingbuju-chen <1172115467@qq.com>
2021-03-16 15:19:51 +0800

    修改内容

--- packages/components/cyf/soqiTest.vue
+++ packages/components/cyf/soqiTest.vue
@@ -1,11 +1,28 @@
 <template>
    <div>
-      这是一个文本公用组件
+      这是一个文本公用组件{{title}}
    </div>
 </template>
-
 <script>
+export default{
+  props: {
+    title:{
+      type:'String',
+      default:'标题'
+    }
+  },
+  data(){
+    return{
 
+    }
+  },
+  methods:{
+
+  },
+  mounted:function(){
+  },
+
+}
 </script>
 
 <style scoped>
